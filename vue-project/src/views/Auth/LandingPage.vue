9384d2d1496ec823a1ba5050ad7e0a783c9d4088
BTisfor <97487586+BTisfor@users.noreply.github.com>
2023-10-30 16:07:37 +0800

    css attempt

--- vue-project/src/views/Auth/LandingPage.vue
+++ vue-project/src/views/Auth/LandingPage.vue
@@ -1,79 +1,64 @@
 <template>
-  <div class="landing-page">
-    <img id="bg" src="@/assets/GetFitt.png" alt="">
-
-    <h1>Welcome to Our App</h1>
-    <form @submit.prevent="login">
-      <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="email" id="email" v-model="email" required>
+  <div :class="$style.landingPage">
+    <div :class="$style.contentContainer">
+      <div :class="$style.form">
+        <div :class="$style.username">
+          <div :class="$style.rectangle" />
+          <div :class="$style.username1">Email:</div>
+          <img :class="$style.userIcon" alt="" src="/user.svg" />
+          <input type="email" id = "email" v-model="email" required />
+        </div>
+        <div :class="$style.password">
+          <div :class="$style.rectangle" />
+          <div :class="$style.password1">Password:</div>
+          <img :class="$style.lockIcon" alt="" src="/lock.svg" />
+          <img
+            :class="$style.passwordHideAndSee"
+            alt=""
+            src="/password-hide-and-see.svg"
+          />
+          <input type="password" id = "password" v-model="password" required /> 
+        </div>
+        <button :class="$style.loginButton" @click="login">Login</button>
+        <button :class="$style.signInButton" @click="signup">Sign Up</button>
+        <div :class="$style.resetPassword" @click="resetPassword">Reset Password</div>
+        <p v-if="errMsg"> {{ errMsg }}</p>
       </div>
-      <div class="form-group">
-        <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" required>
-      </div>
-      <button type="submit" @click="customerLogin">Customer Login</button>
-      <button type="submit" @click="businessLogin">Business Login</button>
-    </form>
-    <p v-if="errMsg"> {{ errMsg }}</p>
-    <button @click="signup">Sign Up</button>
-    <button @click="resetPassword">Reset Password</button>
+    </div>
+    <img :class="$style.getfitt1Icon" alt="" src="@/assets/GetFitt.png" />
   </div>
 </template>
 
 <script>
+import { defineComponent } from "vue";
 import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 
-export default {
+export default defineComponent({
+  name: "LandingPage",
   data() {
     return {
       email: '',
       password: '',
       errMsg: '',
-      userType: '', // Add a userType variable to distinguish between 'customer' and 'business'
     };
   },
   methods: {
     login() {
-      if (this.userType === 'customer') {
-        console.log('Customer logging in...');
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, this.email, this.password)
-          .then(() => {
-            console.log('Customer authenticated.'); // Add this line
-            this.$router.push({ name: 'CustomerHomePage' });
-          })
-          .catch((error) => {
-            // Handle login errors
-            this.handleError(error);
-          });
-      } else if (this.userType === 'business') {
-        console.log('Business logging in...');
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, this.email, this.password)
-          .then(() => {
-            console.log('Business authenticated.'); // Add this line
-            this.$router.push({ name: 'BusinessHomePage' });
-          })
-          .catch((error) => {
-            // Handle login errors
-            this.handleError(error);
-          });
-      } else {
-        this.errMsg = 'Please select user type';
-      }
-    },
-    customerLogin() {
-      this.userType = 'customer';
-      this.login();
-    },
-    businessLogin() {
-      this.userType = 'business';
-      this.login();
+      console.log('Logging in...');
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, this.email, this.password)
+        .then(() => {
+          console.log('Authenticated.'); // Add this line
+          // Replace the following line with appropriate routing logic
+        })
+        .catch((error) => {
+          // Handle login errors
+          this.handleError(error);
+        });
     },
     signup() {
       console.log('Redirecting to sign-up page');
-      this.$router.push({ name: 'SignUpPage' });
+      // Replace the following line with appropriate routing logic
     },
     resetPassword() {
       // Handle password reset
@@ -107,20 +92,171 @@ export default {
       }
     },
   },
-};
+});
 </script>
 
-<style scoped>
-.landing-page {
-  text-align: center;
-  margin: 20px;
-}
 
-.form-group {
-  margin-bottom: 10px;
-}
-
-button {
-  margin: 5px;
-}
+<style module>
+  .contentContainer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .bgIcon {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 1802.5px;
+    height: 1152px;
+  }
+  .rectangle {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: var(--br-9xs);
+    border: 1px solid var(--color-royalblue);
+    box-sizing: border-box;
+    width: 300px;
+    height: 45px;
+  }
+  .username1 {
+    position: absolute;
+    top: 13px;
+    left: 65px;
+    line-height: 20px;
+    text-transform: uppercase;
+    font-weight: 500;
+  }
+  .userIcon {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    width: 25px;
+    height: 25px;
+    overflow: hidden;
+  }
+  .username {
+    position: absolute;
+    top: 0px;
+    left: 1px;
+    width: 300px;
+    height: 45px;
+    font-size: var(--font-size-sm);
+  }
+  .password1 {
+    position: absolute;
+    top: 13px;
+    left: 64px;
+    line-height: 20px;
+    text-transform: uppercase;
+    font-weight: 500;
+  }
+  .lockIcon {
+    position: absolute;
+    height: 47.56%;
+    width: 7.5%;
+    top: 47.56%;
+    right: 88.83%;
+    bottom: 4.89%;
+    left: 3.67%;
+    max-width: 100%;
+    overflow: hidden;
+    max-height: 100%;
+  }
+  .passwordHideAndSee {
+    position: absolute;
+    top: 13px;
+    left: 271px;
+    width: 20px;
+    height: 20px;
+    overflow: hidden;
+  }
+  .password {
+    position: absolute;
+    top: 65px;
+    left: 1px;
+    width: 300px;
+    height: 45px;
+    font-size: var(--font-size-sm);
+  }
+  .rectangle2 {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: var(--br-9xs);
+    background-color: var(--color-white);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
+    width: 300px;
+    height: 45px;
+  }
+  .login {
+    position: absolute;
+    top: 14px;
+    left: 124px;
+    line-height: 20px;
+    text-transform: uppercase;
+    font-weight: 600;
+  }
+  .loginButton {
+    position: absolute;
+    top: 137px;
+    left: 0px;
+    width: 330px;
+    height: 45px;
+  }
+  .signUp {
+    position: absolute;
+    top: 12px;
+    left: 113px;
+    line-height: 20px;
+    text-transform: uppercase;
+    font-weight: 600;
+  }
+  .signInButton {
+    position: absolute;
+    top: 208px;
+    left: 2px;
+    width: 330px;
+    height: 45px;
+  }
+  .resetPassword {
+    position: absolute;
+    top: 279px;
+    left: 89px;
+    color: blue
+  }
+  .form {
+    position: absolute;
+    top: center;
+    left: center;
+    width: 302px;
+    height: 299px;
+  }
+  .getfitt1Icon {
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: auto;
+    max-height: 1000px;
+    object-fit: cover;
+  }
+  .landingPage {
+    display: flex; /* Add flexbox display */
+    align-items: center; /* Vertically center the content */
+    justify-content: center; /* Horizontally center the content */
+    position: relative;
+    background-color: #ff8a00;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    text-align: center;
+    font-size: var(--font-size-base);
+    color: var(--color-royalblue);
+    font-family: var(--font-montserrat);
+  }
 </style>
