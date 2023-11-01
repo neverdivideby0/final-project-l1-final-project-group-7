27a98428ec00bf68ea05439a0976589289f942e7
BTisfor <97487586+BTisfor@users.noreply.github.com>
2023-11-02 01:14:43 +0800

    fixed again

--- vue-project/src/views/Auth/LandingPage.vue
+++ vue-project/src/views/Auth/LandingPage.vue
@@ -1,10 +1,7 @@
 <template>
   <div :class="$style.landingPage">
-
+    <img :class="$style.bgIcon" src="@/assets/GetFitt.png" alt="">
     <form @submit.prevent="login" :class="$style.form">
-      <div :class="$style.bgIcon">
-        <img :class="$style.bgIcon" src="@/assets/GetFitt.png" alt="">
-      </div>
       <div :class="$style.username">
         <input type="email" id="email" v-model="email" required placeholder="Email"/>
       </div>
@@ -126,12 +123,12 @@ export default {
     align-items: center;
     justify-content: center;
     position: absolute;
-    top: 30%;
+    top: 25%;
     left: 50%;
     transform: translate(-50%, -50%);
     width: 1802.5px;
     height: 1152px;
-    margin-bottom: 20px;
+     margin-bottom: 20px;
   }
   .rectangle {
     position: absolute;
@@ -142,58 +139,6 @@ export default {
     box-sizing: border-box;
     width: 300px;
     height: 45px;
-  }
-  .username1 {
-    position: absolute;
-    top: 13px;
-    left: 65px;
-    line-height: 20px;
-    text-transform: uppercase;
-    font-weight: 500;
-  }
-  .userIcon {
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    width: 25px;
-    height: 25px;
-    overflow: hidden;
-  }
-  .username1 {
-    position: flex;
-    top: 13px;
-    left: 10px;
-    width: 0px;
-    height: 45px;
-    font-size: var(--font-size-sm);
-  }
-  .password1 {
-    position: absolute;
-    top: 13px;
-    left: 10px;
-    line-height: 20px;
-    text-transform: uppercase;
-    font-weight: 500;
-  }
-  .lockIcon {
-    position: absolute;
-    height: 47.56%;
-    width: 7.5%;
-    top: 47.56%;
-    right: 88.83%;
-    bottom: 4.89%;
-    left: 3.67%;
-    max-width: 100%;
-    overflow: hidden;
-    max-height: 100%;
-  }
-  .passwordHideAndSee {
-    position: absolute;
-    top: 13px;
-    left: 271px;
-    width: 20px;
-    height: 20px;
-    overflow: hidden;
   }
 
   .password {
@@ -257,7 +202,7 @@ export default {
     top: center;
     left: center;
     width: 300px;
-    height: 29px;
+    height: 299px;
   }
 
   .landingPage {
